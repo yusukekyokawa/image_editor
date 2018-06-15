142c1ee64fe5020a4549656b4295ca1746ba0be7
yusukekyokawa <medakasuisui598@gmail.com>
2018-06-15 19:21:05 +0900

    画像の切り取り

--- cut_image.py
+++ cut_image.py
@@ -1,21 +1,53 @@
-import cv2
 import numpy as np
+import cv2
+from image_prepro import padding_position,to_grayscale,blur, resize_image,binary_threshold
 
-def resize_image(img, size):
-    # size is enough to img
-    img_size = img.shape[:2]
-    if img_size[0] > size[1] or img_size[1] > size[0]:
-        raise Exception("img is larger than size")
+def main():
+    img = cv2.imread("./images/"+"arcKG"+"00001"+".jpg")
+    ret = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+    blur_img = blur(ret)
+    cv2.imshow("katagmai", blur_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # centering
-    row = (size[1] - img_size[0]) // 2
-    col = (size[0] - img_size[1]) // 2
-    resized = np.zeros(list(size) + [img.shape[2]], dtype=np.uint8)
-    resized[row:(row + img.shape[0]), col:(col + img.shape[1])] = img
 
-    # filling
-    mask = np.full(size, 255, dtype=np.uint8)
-    mask[row:(row + img.shape[0]), col:(col + img.shape[1])] = 0
-    filled = cv2.inpaint(resized, mask, 3, cv2.INPAINT_TELEA)
 
-    return filled
+
+
+
+
+"""#画像の輪郭検出
+def detect_contour(img, min_size):
+    #contoured = cv2.imread(path)
+    forcrop = cv2.imread(path)
+
+    # make binary image　　画像の2値か
+    img = binary_threshold(path)
+    img = cv2.bitwise_not(img)
+
+    # detect contour
+    im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    crops = []
+    # draw contour
+    for c in contours:
+        if cv2.contourArea(c) < min_size:
+            continue
+
+        # rectangle area
+        x, y, w, h = cv2.boundingRect(c)
+        x, y, w, h = padding_position(x, y, w, h, 5)
+
+        # crop the image
+        cropped = forcrop[y:(y + h), x:(x + w)]
+        cropped = resize_image(cropped, (210, 210))
+        crops.append(cropped)
+
+        # draw contour
+        cv2.drawContours(contoured, c, -1, (0, 0, 255), 3)  # contour
+        cv2.rectangle(contoured, (x, y), (x + w, y + h), (0, 255, 0), 3)  #rectangle contour
+
+    return contoured, crops"""
+
+if __name__ =="__main__":
+    main()
